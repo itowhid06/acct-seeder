8d4539c194f2d2e0f8241c51df588ec5d159dc62
nadim1992 <nadim0988@gmail.com>
2019-05-22 11:01:06 +0600

    Rename opening balance table name

--- includes/class-acct-seeder.php
+++ includes/class-acct-seeder.php
@@ -62,7 +62,7 @@ class Acct_Seeder {
 			'erp_acct_ledger_settings',
 			'erp_acct_ledgers',
 			'erp_acct_financial_years',
-			'erp_acct_opening_balance',
+			'erp_acct_opening_balances',
 			'erp_acct_pay_bill',
 			'erp_acct_pay_bill_details',
 			'erp_acct_pay_purchase',
