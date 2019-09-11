8be4cdf037a562687be120ccd4019abec46231fc
nadim1992 <nadim0988@gmail.com>
2019-09-11 11:13:59 +0600

    add people trn detail table

--- includes/class-acct-seeder.php
+++ includes/class-acct-seeder.php
@@ -70,6 +70,7 @@ class Acct_Seeder {
 			'erp_acct_payment_methods',
 			'erp_acct_people_account_details',
 			'erp_acct_people_trn',
+			'erp_acct_people_trn_details',
 			'erp_acct_product_categories',
 			'erp_acct_product_details',
 			'erp_acct_product_types',
